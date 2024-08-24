832c32664e5dedb072e42071121053a7470c7c41
Maurice Ambroise <maurice.ambroise79@gmail.com>
2024-08-24 13:58:52 +0200

    upgrade logout

--- Djangoproject/facturasieli/views/registration/log_out_view.py
+++ Djangoproject/facturasieli/views/registration/log_out_view.py
@@ -6,6 +6,7 @@
 # ---------------------------------------------------------------------------
 
 from django.contrib.auth import logout
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -18,5 +19,6 @@ def log_out(request: HttpRequest):
 
     if request.method == 'POST':
         logout(request)
-        return HttpResponseRedirect(reverse('facturasieli:goodbye'))
+        messages.success(request,_('You have been successfully logged out. We hope to see you again soon!'))
+        return HttpResponseRedirect(reverse('facturasieli:index'))
     return render(request, 'registration/logout.html')
